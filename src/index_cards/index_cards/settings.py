bd24ee39d512632b89dfd937c4a47af5419e2a77
mderler <5matthiasderler5@gmail.com>
2023-01-12 20:18:11 +0100

    moved project, created new secret key, added flake8 packages

--- src/index_cards/index_cards/settings.py
+++ src/index_cards/index_cards/settings.py
@@ -12,15 +12,16 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from secret import SRC_SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d*cb0rsosa=lo1eh0mf*63-ge7+m(#3-l6e6qv2!9#at^h*^q_'
+SECRET_KEY = SRC_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
