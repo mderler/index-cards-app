d72594b8b57b21537dac24530d8d2e2317fa7834
mderler <derler190063@sr.htlweiz.at>
2023-01-19 17:30:52 +0100

    rest api

--- src/index_cards/api/views.py
+++ src/index_cards/api/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Topic
+from .serializers import TopicSerializer
 
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def topic_list(request):
+    if request.method == 'GET':
+        topics = Topic.objects.all()
+        serializer = TopicSerializer(topics, many=True)
+        return JsonResponse({"data": serializer.data})
+
+    if request.method == 'POST':
+        serializer = TopicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def topic_detail(request, pk):
+    try:
+        topic = Topic.objects.get(pk=pk)
+    except Topic.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = TopicSerializer(topic)
+        return JsonResponse({"data": serializer.data})
+
+    if request.method == 'PUT':
+        serializer = TopicSerializer(topic, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        pass
