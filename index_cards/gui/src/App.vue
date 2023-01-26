48caad13c35aa052360adb80f5f8ee6bdfe5838a
mderler <derler190063@sr.htlweiz.at>
2023-01-26 17:30:35 +0100

    remove default page

--- index_cards/gui/src/App.vue
+++ index_cards/gui/src/App.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<template></template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
