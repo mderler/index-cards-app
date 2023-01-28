4dd593bc071534367a132e0a32b74506f3a043f0
mderler <derler190063@sr.htlweiz.at>
2023-01-28 23:00:24 +0100

    changed serializer key names

--- index_cards/api/serializers.py
+++ index_cards/api/serializers.py
@@ -7,12 +7,24 @@ class TopicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Topic
         fields = ['id', 'topic_name']
-
+    
+    def to_representation(self, instance: Topic):
+        return {
+            'id': instance.pk,
+            'topicName': instance.topic_name
+        }
+        
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
         fields = ['id', 'topic', 'question', 'answer']
+
+    def to_representation(self, instance: Card):
+        representation = super().to_representation(instance)
+        del(representation['topic'])
+        representation['topicId'] = instance.topic.pk
+        return representation
 
 
 class PractiseSessionSerializer(serializers.ModelSerializer):
@@ -20,8 +32,24 @@ class PractiseSessionSerializer(serializers.ModelSerializer):
         model = PractiseSession
         fields = ['id', 'topic', 'session_start']
 
+    def to_representation(self, instance: PractiseSession):
+        return {
+            'id': instance.pk,
+            'topicId': instance.topic.pk,
+            'sessionStart': instance.session_start
+        }
+
 
 class SessionCardSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionCard
         fields = ['id', 'card', 'practise_session', 'user_answer', 'correct']
+    
+    def to_representation(self, instance: SessionCard):
+        return {
+            'id': instance.pk,
+            'cardId': instance.card.pk,
+            'practiseSessionId': instance.practise_session.pk,
+            'userAnswer': instance.user_answer,
+            'correct': instance.correct
+        }
